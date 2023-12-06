ae59554548b6885d7b60ab32952ceae50fc85bea
Adeegithub <arunodagunawardana@gmail.com>
2023-12-06 15:30:19 +0530

    Changed Testing Edit Teacher*

--- frontend/test/teacher-edit.test.js
+++ frontend/test/teacher-edit.test.js
@@ -3,30 +3,21 @@ process.env.NODE_ENV = "test";
 
 fixture`Testing Teacher UI`
     .page`http://localhost:4401/`
-    test('Testing edit teachers', async t => {
-        await t.navigateTo("/");
-        await t.click("#teacher-edit-10003");
+test('Testing edit teachers', async t => {
     
-        await t.typeText("#teacher-name", "Changed Teacher Name", { replace: true });
-        await t.typeText("#teacher-age", "99", { replace: true });
-        await t.click("#teacher-edit");
-    
-        // Wait for the update operation to complete and UI to refresh
-        await t.wait(1000); // Adjust the waiting time as needed
-    
-        await t.navigateTo("/");
-    
-        // Check the entire table content for debugging
-        const tableContent = await Selector('#teacher-table').innerText;
-        console.log(tableContent);
-    
-        // Adjust this selector based on the actual DOM structure
-        const editedTeacherSelector = Selector('td').withText('10003').parent('tr');
-        const editedTeacherRow = await editedTeacherSelector.innerText;
-    
-        await t.expect(editedTeacherRow).contains("Changed Teacher Name");
-    
-        await t.click("#teacher-delete-10003");
-    });
-    
-    
+    await t.click("#teacher-edit-10003");
+
+    await t.typeText("#teacher-name", "Changed Teacher Name");
+    await t.typeText("#teacher-age", "99");
+    await t.click("#teacher-edit");
+
+    await t.navigateTo("/");
+
+    const table = Selector('#teacher-table')
+    const rowCount = await table.find('tr').count;
+
+    let tdText = await table.find('tr').nth(rowCount - 1).innerText;
+    await t.expect(tdText).contains("Changed Teacher Name");
+
+    await t.click("#teacher-delete-10003");
+});
